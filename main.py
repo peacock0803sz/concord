816dfc4361ad927b9304c24325a3598163f33884
peacock <peacock0803sz@gmail.com>
2019-10-31 14:48:46 +0900

    :poop: Delete token

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 import discord.ext.commands
 import observer
 import point
 
 if __name__ == '__main__':
-    BOT_TOKEN = 'NTQyNjM5MzQ3NDgxMDUxMTM2.XbBVpA.TaLelksqmyCKTjDVYAcK3BHEYtk'
+    BOT_TOKEN = os.environ['CONCORD_TOKEN']
     bot = discord.ext.commands.Bot('/')
     # bot.add_cog(observer.Observer(bot))
     bot.add_cog((point.Count(bot)))
